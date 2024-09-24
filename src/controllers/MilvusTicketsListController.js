2933717131cb1874c0c6ba335658416a1b195fe9
AlexsandroVSantos <alexsandro.santos@conab.com.br>
2024-09-24 11:15:13 -0300

    Update the data of the email to be sent
    The email sent included a ticket count, with the tickets sorted by day from highest to lowest.

--- src/controllers/MilvusTicketsListController.js
+++ src/controllers/MilvusTicketsListController.js
@@ -1,5 +1,6 @@
 const nodemailer = require("nodemailer");
 const axios = require("axios");
+const moment = require("moment");
 
 axios.defaults.headers.common["Authorization"] =
   "uTLcDZAvKaEavsKRJJMwxQmuCvtbU5iExCfI1kx52hAul46yDWVbumu0wLJN6dPyzXgKw7cAncjgp4jSKJ0lvaFIun6uypfiyzaCy";
@@ -63,13 +64,21 @@ function groupByUser(tickets) {
       acc[contato] = [];
     }
 
-    const { codigo, assunto, motivo_pausa } = ticket;
+    const {
+      codigo,
+      assunto,
+      motivo_pausa,
+      data_criacao,
+      ultima_log: { data: ultima_log_data },
+    } = ticket;
 
     if (motivo_pausa === "PENDENTE CLIENTE") {
       acc[contato].push({
         codigo: codigo,
         assunto: assunto,
         motivo_pausa: motivo_pausa,
+        data_criacao: data_criacao,
+        ultima_log_data: ultima_log_data, // Melhor manter o nome mais descritivo
       });
     }
 
@@ -78,7 +87,7 @@ function groupByUser(tickets) {
 }
 
 function sendEmail(user, tickets) {
-  if (user !== "alex") {
+  if (user !== "fabio") {
     return;
   }
 
@@ -94,14 +103,30 @@ function sendEmail(user, tickets) {
     //}
   });
 
-  // console.log(groupedTickets);
+  const dataAtualServidor = moment();
+
+  // Ordenar tickets pela diferença de dias sem interação (do maior para o menor)
+  const orderedTickets = tickets
+    .map((ticket) => {
+      const diasSemInteracao = dataAtualServidor.diff(
+        moment(ticket.ultima_log_data || ticket.data_criacao),
+        "days"
+      );
+      return {
+        ...ticket,
+        diasSemInteracao,
+      };
+    })
+    .sort((a, b) => b.diasSemInteracao - a.diasSemInteracao); // Ordenação decrescente
 
   // Criando a tabela com os dados
-  const tableRows = tickets
+  const tableRows = orderedTickets
     .map((ticket) => {
       return `
       <tr>
         <td>${ticket.codigo}</td>
+        <td>${moment(ticket.data_criacao).format("DD/MM/YYYY")}</td>
+        <td>${ticket.diasSemInteracao}</td>
         <td>${ticket.assunto}</td>
         <td>${ticket.motivo_pausa}</td>
       </tr>
@@ -109,19 +134,24 @@ function sendEmail(user, tickets) {
     })
     .join(""); // .join('') para juntar todas as linhas da tabela em uma string única
 
+  // Contador total de tickets
+  const totalTickets = orderedTickets.length;
+
   const htmlContent = `
   <div  style="color: red;"><b>[Mensagem automática, por favor, não responda a esse e-mail]</b></div>
   <br/>
-  <div>Olá, ${capitalizeFirstLetter(user)}, tudo bem? </div>
+  <div>Olá, <b>${capitalizeFirstLetter(user)} </b>, tudo bem? </div>
   <br/>
   <div>Os tickets listados abaixo aguardam sua interação no Milvus. Sua resposta é crucial para avançarmos na resolução dos casos ou encerrarmos o atendimento, permitindo o seguimento para outras solicitações, dúvidas e correções.</div>
   <br/>
 
-   <h2>Chamados pendentes de interação</h2>
+   <h2>Tickets pendentes de interação</h2>
   <table border="1" cellpadding="10" cellspacing="0">
     <thead>
       <tr>
         <th>N°</th>
+        <th>Data Criação</th>
+        <th>Dias sem interação</th>
         <th>Assunto</th>
         <th>Situação</th>
       </tr>
@@ -130,12 +160,15 @@ function sendEmail(user, tickets) {
       ${tableRows}
     </tbody>
   </table>
+  <br/>
+    <div>Total de tickets: <b>${totalTickets}</b></div> <!-- Adicionando o contador total -->
 `;
 
   const mailOptions = {
     from: "alexsandro.santos@conab.com.br",
-    to: "web_alexsandro@hotmail.com",
-    subject: "Chamados pendentes de interação [Mensagem automática]",
+    to: "marcelo.pimentel@conab.com.br",
+    // to: "alexsandro.santos@conab.com.br",
+    subject: "Tickets pendentes de interação [Mensagem automática]",
     html: htmlContent, // Corpo do e-mail com HTML (tabela)
   };
 
