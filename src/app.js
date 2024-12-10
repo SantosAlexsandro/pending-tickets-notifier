24d7cb8fc12d6d22f53d131921f5235e36be5665
SantosAlexsandro <web_alexsandro@hotmail.com>
2024-12-10 16:33:30 -0300

    Feat: Send peding tickets from XBotDesk by email

--- src/app.js
+++ src/app.js
@@ -1,56 +1,31 @@
 import dotenv from "dotenv";
-import { resolve } from "path";
-import cors from "cors";
 import cron from "node-cron";
+import express from "express";
 const MilvusTicketsList = require("../src/controllers/MilvusTicketsListController");
+const RiosoftTicketsList = require("../src/controllers/RiosoftTicketsListController");
 
 dotenv.config();
-
-import express from "express";
-// import tokenRoutes from './routes/tokenRoutes';
-import milvusTicketsListRoutes from "./routes/milvusTicketsListRoutes";
-
-const whiteList = ["http://localhost:3000", "https://dash.cumpacell.com"];
-
-const corsOptions = {
-  origin(origin, callback) {
-    if (whiteList.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
 
 class App {
   constructor() {
     this.app = express();
-    this.middlewares();
-    this.routes();
     this.startCronJob();
   }
 
-  middlewares() {
-    this.app.use(cors(corsOptions));
-    // this.app.use(helmet());
-    this.app.use(express.urlencoded({ extended: true }));
-    this.app.use(express.json());
-  }
-
-  routes() {
-    //this.app.use("/", milvusTicketsListRoutes);
-    // this.app.use('/tokens/', tokenRoutes);
-  }
-
   startCronJob() {
-    // Configura o cron job para rodar a cada minuto
+    // Milvus
     cron.schedule("30 7 * * 1,2,3,4,5", () => {
-      console.log("Executando diariamente as 7:30");
-      MilvusTicketsList.executeTask(); // Chama a função sem req e res
+      console.log("Executando envio de Tickets do Milvus");
+      MilvusTicketsList.executeTask();
     });
+
+    // XBotDesk
+    cron.schedule("45 7 * * 1,2,3,4,5", () => {
+      console.log("Executando envio de Tickets do XBotDesk");
+      RiosoftTicketsList.executeTask();
+    });
+
   }
 }
-
-//  41 7 * * *
 
 export default new App().app;
---
+++ src/controllers/RiosoftTicketsListController.js
@@ -0,0 +1,159 @@
+const nodemailer = require("nodemailer");
+const axios = require("axios");
+const moment = require("moment");
+require("dotenv").config();
+
+const urlBase = "http://riosoft.xbotdesk.com.br/api";
+
+const ticketsListUrl =
+  urlBase +
+  "/Ticket/RetrievePage?filter=IDEstado=10&order=&pageSize=20&pageIndex=1";
+
+const tokenApiUrl = urlBase + "/RsLogin/Login";
+
+const payload = {
+  UserName: "CONAB.ALEXSANDROSANTOS",
+  Password: "Al8725@",
+};
+
+// Função para obter o token
+async function fetchTokenByUserName() {
+  try {
+    const tokenResponse = await axios.post(tokenApiUrl, payload);
+    console.log("tokenResponse", tokenResponse.headers["riosoft-token"]);
+    if (tokenResponse.headers["riosoft-token"]) {
+      return tokenResponse.headers["riosoft-token"];
+    } else {
+      throw new Error("Token não encontrado na resposta");
+    }
+  } catch (e) {
+    console.log("Erro ao obter o token", e);
+    throw e; // Lança o erro para ser capturado posteriormente
+  }
+}
+
+// Função para configurar o cabeçalho do Axios com o token
+async function setupAxiosHeader() {
+  try {
+    axios.defaults.headers.common["Riosoft-Token"] =
+      await fetchTokenByUserName();
+  } catch (e) {
+    console.error("Erro ao configurar o token no axios", e);
+  }
+}
+
+// Chama a função para buscar a lista de tickets
+async function fetchTicketsList() {
+  console.log("Iniciando a tarefa de envio de tickets...");
+  try {
+    await setupAxiosHeader(); // Configura o token primeiro
+    const ticketsResponse = await axios.get(ticketsListUrl);
+    const tickets = ticketsResponse.data;
+    console.log("tickets recebidos", tickets.length);
+    return tickets;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+// Função para enviar e-mails com os tickets pendentes
+async function sendEmail(tickets) {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+    tls: { ciphers: "SSLv3", rejectUnauthorized: false },
+    connectionTimeout: 60000,
+  });
+
+  const dataAtualServidor = moment();
+
+  // Calcular "dias sem interação" com base em 'ultima_log.data'
+  const orderedTickets = tickets
+    .map((ticket) => {
+      const ultimaInteracao = ticket.DataHoraAlteracaoEstado;
+      const diasSemInteracao = dataAtualServidor.diff(
+        moment(ultimaInteracao),
+        "days"
+      );
+      return {
+        ...ticket,
+        diasSemInteracao,
+      };
+    })
+    .sort((a, b) => b.diasSemInteracao - a.diasSemInteracao);
+
+  // Criar a tabela com os tickets
+  const tableRows = orderedTickets
+    .map(
+      (ticket) => `
+  <tr>
+   <td>${ticket.NumeroTicket}</td>
+    <td>${moment(ticket.DataHoraCadastro).format("DD/MM/YYYY")}</td>
+    <td style="text-align: center;">${ticket.diasSemInteracao}</td>
+    <td>${ticket.Titulo}</td>
+  </tr>`
+    )
+    .join("");
+
+  const totalTickets = tickets.length;
+
+  // Montar o conteúdo do e-mail usando o layout fornecido
+  const htmlContent = `
+<div style="color: red;">
+</div>
+<br/>
+<div>Olá, Marcelo, tudo bem?</div>
+<br/>
+<div>Os tickets listados abaixo estão pendentes de retorno por parte do fornecedor.</div>
+<br/>
+<h2>Tickets pendentes de retorno do fornecedor</h2>
+<table border="1" cellpadding="10" cellspacing="0">
+  <thead>
+    <tr>
+      <th>N°</th>
+      <th>Data criação</th>
+      <th>Dias sem retorno</th>
+      <th>Assunto</th>
+    </tr>
+  </thead>
+  <tbody>${tableRows}</tbody>
+</table>
+<br/>
+ <div>Total de tickets: <b>${totalTickets}</b></div>
+`;
+
+  // Configurar e enviar o e-mail
+  await transporter.sendMail({
+    from: "cpd4@conab.com.br",
+    to: "marcelo.pimentel@conab.com.br",
+    bcc: "alexsandro.santos@conab.com.br", // E-mails em cópia oculta
+    subject: `Tickets pendentes de retorno do fornecedor Riosoft`,
+    html: htmlContent,
+  });
+
+  console.log(`E-mail enviado`);
+}
+
+class RiosoftTicketsList {
+  async executeTask() {
+    try {
+      const tickets = await fetchTicketsList(); // Espera pela lista de tickets
+      if (tickets && tickets.length > 0) {
+        await sendEmail(tickets);
+      } else {
+        console.log("Nenhum ticket para enviar.");
+      }
+    } catch (e) {
+      console.error("Erro ao executar a tarefa de tickets:", e);
+    }
+  }
+}
+
+const riosoftTicketsList = new RiosoftTicketsList();
+
+module.exports = riosoftTicketsList;
